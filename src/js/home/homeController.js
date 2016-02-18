31d0eb9188aa874a19d3f612ef8816157d725905
andreasmaier <andreas.maier@gmail.com>
2016-02-17 23:30:44 -0800

    Add Todo and show it in the list

--- src/js/home/homeController.js
+++ src/js/home/homeController.js
@@ -2,4 +2,12 @@ angular.module('todo.home').controller('HomeController', function ($scope) {
     $scope.message = 'These are your Todos:';
 
     $scope.todos = [];
+
+    $scope.addTodo = function () {
+        $scope.todos.push({
+            name: $scope.newTodo
+        });
+
+        $scope.newTodo = '';
+    }
 });
--- tests/e2e/flowTest.js
+++ tests/e2e/flowTest.js
@@ -8,6 +8,7 @@ describe('CashLoad Flow', function () {
 
     userSeesHomeScrees();
     userSeesNoTodos();
+    userAddsNewTodo();
 
     function userSeesHomeScrees() {
         it('takes the user to the home page by default', function () {
@@ -17,9 +18,24 @@ describe('CashLoad Flow', function () {
 
     function userSeesNoTodos() {
         it('does not have any Todos by default', function () {
-            element.all(by.repeater('todo in todos')).count().then(function(count) {
-                expect(count).toBe(0);
-            });
+            expect(element(by.repeater('todo in todos')).$$().count()).toBe(0);
+        });
+    }
+
+    function userAddsNewTodo() {
+        it('shows the todo in the list', function () {
+            element(by.model('newTodo')).sendKeys('Clean up!');
+            element(by.css('.add-todo-button')).click();
+
+            expect(element.all(by.repeater('todo in todos')).count()).toBe(1);
+        });
+
+        it('clears the input text field', function () {
+            expect(element(by.model('newTodo')).getAttribute('value')).toBe('');
+        });
+
+        it('show the new Todo in the list', function () {
+            expect(element.all(by.repeater('todo in todos')).get(0).getText()).toBe('Clean up!');
         });
     }
 });
