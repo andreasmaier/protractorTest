e41d50343d7778bcdc983e4d8075be8ee42753fe
Andreas Maier <andreas.maier@capitalone.com>
2016-02-18 18:05:17 -0800

    Add e2e test script (starts web driver, server waits for selenium and runs tests)

---
+++ run_e2e_tests.sh
@@ -0,0 +1,16 @@
+webdriver-manager start > /dev/null 2>&1 &
+
+WEBDRIVER_PID=$!
+
+node ./bin/server.js > /dev/null 2>&1 &
+
+SERVER_PID=$!
+
+while ! nc -vz localhost 4444; do sleep 1; done
+ 
+echo 'Selenium-RC server is open for business on port 4444!'
+
+protractor protractor.conf.js
+
+curl -s -L http://localhost:4444/selenium-server/driver?cmd=shutDownSeleniumServer
+kill $SERVER_PID
